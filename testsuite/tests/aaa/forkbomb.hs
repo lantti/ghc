b4bebe51034d4ae25669b49912d66c64772d10da
Antti Siponen <antti.siponen@iki.fi>
2018-08-19 00:27:49 +0200

    #15363 Make forkbomb testcase nastier. Revert when no longer needed

--- testsuite/tests/aaa/forkbomb.hs
+++ testsuite/tests/aaa/forkbomb.hs
@@ -9,7 +9,8 @@ dimensions = [3,2,1]
 
 explode :: [String] -> IO ()
 explode [] = explode (map show dimensions)
-explode (x:[]) = explode' (read x) "sleep" ["600"]
+explode ("leaf":[]) = forever (return ())
+explode (x:[]) = explode' (read x) "./forkbomb" ["leaf"]
 explode (x:xs) = explode' (read x) "./forkbomb" xs
 
 explode' n c a = replicateM n (spawnProcess c a) >>= (mapM_ waitForProcess)
